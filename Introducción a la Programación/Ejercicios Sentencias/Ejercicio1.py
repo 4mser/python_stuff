0f69dccad1f432e57522c3c1c4255972e471e205
4mser <nicolas.moreno.sagan@gmail.com>
2023-04-12 22:13:53 -0400

    Cambios ejercicios

--- Introducción a la Programación/Ejercicios Sentencias/Ejercicio1.py
+++ Introducción a la Programación/Ejercicios Sentencias/Ejercicio1.py
@@ -1,32 +1,4 @@
-""" # Input 2 enteros
-n1 = int(input('Ingrese un número entero: '))
-n2 = int(input('Ingrese otro número Entero: '))
-
-#Resultados
-r_suma = int(n1 + n2)
-r_resta = int(n1 - n2)
-r_mult = int(n1 * n2)
-
-
-print('Los resultados de las operaciones básicas son: ')
-print(f'Suma: {r_suma}')
-print('Resta: ', r_resta)
-print('Multiplicación: ', r_mult)
-
-
-if n2 == 0:
-    print('Mod: No se puede dividir por 0')
-    print('Mod: No se puede calcular el mod')
-    
-else: 
-    r_div = float(n1 / n2)
-    r_mod = int(n1 % n2)
-    
-    print('División: ', r_div)
-    print('Mod: ', r_mod)
- """
- 
- # Input 2 enteros
+# Input 2 enteros
 n1 = int(input('Ingrese un número entero: '))
 n2 = int(input('Ingrese otro número Entero: '))
 
--- Introducción a la Programación/Ejercicios Sentencias/Ejercicio3.py
+++ Introducción a la Programación/Ejercicios Sentencias/Ejercicio3.py
@@ -2,19 +2,6 @@
 # Si el sueldo es mayor a $1.000.000 tiene un bono del 25% del sueldo.
 # En caso contrario, el bono será del 50%
 
-""" nombre = input('Ingrese su nombre: ')
-sueldo = int(input('Ingrese su sueldo: '))
-
-if sueldo > 1000000:
-    bono = int(sueldo*0.25)
-    sueldo_final = int(sueldo + bono)
-    print(f'{nombre}, recibirá un bono de ${bono}, por lo que recibirá un total de ${sueldo_final}')
-else:
-    bono = int(sueldo*0.5)
-    sueldo_final = int(sueldo + bono)
-    print(f'{nombre}, recibirá un bono de ${bono}, por lo que recibirá un total de ${sueldo_final}')
-    
-      """
 nombre = input('Ingrese su nombre: ')
 sueldo = int(input('Ingrese su sueldo: '))
 
